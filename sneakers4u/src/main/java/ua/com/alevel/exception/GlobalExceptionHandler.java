593cf5f6307e2fd47112781d56e92c063b00ba20
javenier <dvoynos.nikita272003@gmail.com>
2022-01-31 03:03:21 +0200

    add exception handling

---
+++ sneakers4u/src/main/java/ua/com/alevel/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package ua.com.alevel.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {RuntimeException.class, DuplicateItemInOrderException.class, EntityNotFoundException.class})
+    public ModelAndView defaultErrorHandler(RuntimeException exception) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("showMessage", true);
+        mav.addObject("errorMessage", exception.getMessage());
+        mav.setViewName("error");
+        return mav;
+    }
+}
